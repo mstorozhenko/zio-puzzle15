4d9fd10805c98d20e3fd50f7ae7674e6345a79bb
Maksym Storozhenko <mstorozhenko@griddynamics.com>
2021-08-15 10:38:29 +0300

    added ZManaged for console read

--- src/main/scala/com/madimaxi/puzzle15/console/io/impl/TerminalImpl.scala
+++ src/main/scala/com/madimaxi/puzzle15/console/io/impl/TerminalImpl.scala
@@ -3,16 +3,14 @@ package com.madimaxi.puzzle15.console.io.impl
 import com.madimaxi.puzzle15.console.io.Terminal
 import jline.console.ConsoleReader
 import zio.console.Console
-import zio.{Has, Task, URLayer}
+import zio.{Has, Task, URLayer, ZManaged}
 
 case class TerminalImpl(console: Console.Service) extends Terminal {
-  override def read(): Task[Char] = Task.effectTotal {
-    //standard zio have only readLine, jline can readCharacter without pressing enter
-    val console = new ConsoleReader()
-    val l = console.readCharacter().toChar
-    console.close()
-    l
-  }
+  override def read(): Task[Char] =
+  //standard zio have only readLine, jline can readCharacter without pressing enter
+    ZManaged.makeEffect(new ConsoleReader())(t => t.close()).use(
+      reader => Task.succeed(reader.readCharacter().toChar)
+    )
 
 
   override def print(s: String): Task[Unit] =
