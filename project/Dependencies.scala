fa1feaab3790ca5bcc13d6eb210a70dbfe659a3f
Maksym Storozhenko <mstorozhenko@griddynamics.com>
2021-08-16 02:35:45 +0300

    fmt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,14 @@
 import sbt._
 
 object Dependencies {
+
+  //console
   val jline = "jline" % "jline" % "2.14.6"
+
+  //zio
   val zio = "dev.zio" %% "zio" % "1.0.10"
+
+  //test
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9" % "test"
   val mockito = "org.scalatestplus" %% "mockito-3-4" % "3.2.9.0" % "test"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,4 @@
 addSbtPlugin("org.jetbrains" % "sbt-ide-settings" % "1.1.0")
+
+// scalafmt
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.3")
