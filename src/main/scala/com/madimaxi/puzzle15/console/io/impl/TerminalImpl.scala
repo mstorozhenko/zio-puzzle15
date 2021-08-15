aae07c827bb1c6b38736ddfa520eb8a727835801
Maksym Storozhenko <mstorozhenko@griddynamics.com>
2021-08-16 02:21:14 +0300

    scalafmt

--- src/main/scala/com/madimaxi/puzzle15/console/io/impl/TerminalImpl.scala
+++ src/main/scala/com/madimaxi/puzzle15/console/io/impl/TerminalImpl.scala
@@ -7,11 +7,11 @@ import zio._
 
 case class TerminalImpl(console: Console.Service) extends Terminal {
   override def read(): UIO[Char] =
-  //standard zio have only readLine, jline can readCharacter without pressing enter
-    ZManaged.makeEffect(new ConsoleReader())(t => t.close()).use(
-      reader => Task.succeed(reader.readCharacter().toChar)
-    ).orDie
-
+    //standard zio have only readLine, jline can readCharacter without pressing enter
+    ZManaged
+      .makeEffect(new ConsoleReader())(t => t.close())
+      .use(reader => Task.succeed(reader.readCharacter().toChar))
+      .orDie
 
   override def print(s: String): UIO[Unit] = console.putStrLn(s).orDie
 
