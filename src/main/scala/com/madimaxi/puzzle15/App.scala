96068bdce1a166802c9c150555efe66350ab558b
Maksym Storozhenko <mstorozhenko@griddynamics.com>
2021-08-18 18:39:27 +0300

    rewrite main using Schedule

--- src/main/scala/com/madimaxi/puzzle15/App.scala
+++ src/main/scala/com/madimaxi/puzzle15/App.scala
@@ -5,23 +5,25 @@ import com.madimaxi.puzzle15.console.io.impl.TerminalImpl
 import com.madimaxi.puzzle15.controller.impl.ControllerImpl
 import com.madimaxi.puzzle15.controller.run.Runner
 import com.madimaxi.puzzle15.controller.run.impl.RunnerImpl
-import com.madimaxi.puzzle15.controller.{Start, State}
+import com.madimaxi.puzzle15.controller.{Start, State, Terminate}
 import com.madimaxi.puzzle15.game.impl.IntBoardControl
 import zio._
 
 object App extends zio.App {
 
-  val program: ZIO[Has[Runner[Int]], Throwable, Any] = {
-    def loop(state: State): ZIO[Has[Runner[Int]], Throwable, Any] =
-      Runner
-        .start[Int](state)
-        .foldM(
-          _ => UIO.unit,
-          r => loop(r)
-        )
+//  val program: ZIO[Has[Runner[Int]], Throwable, Any] = {
+//    def loop(state: State): ZIO[Has[Runner[Int]], Throwable, Any] =
+//      Runner
+//        .start[Int](state)
+//        .foldM(
+//          _ => UIO.unit,
+//          r => loop(r)
+//        )
+//
+//    loop(Start)
+//  }
 
-    loop(Start)
-  }
+  val program: ZIO[Has[Runner[Int]], Unit, State] = ZIO.iterate(Start: State)(_ != Terminate)(Runner.start[Int])
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     program
